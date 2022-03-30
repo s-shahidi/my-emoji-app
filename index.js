f06045d6cd3a3e8cfd0de7414e031755db79f707
s-shahidi <57652765+s-shahidi@users.noreply.github.com>
2022-03-29 21:10:33 -0700

    Added emojis
    Added emojis for software dev, baking, yoga, photography, travel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const myEmojis = ["👩🏻‍💻","🧁","🧘🏻‍♀️","📷","🗺️"]
 
 function renderEmojis() {
     const emojiContainer = document.getElementById("emoji-container")
